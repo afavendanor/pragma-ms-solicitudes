2209cf663c783da84c7ceea658b0913f360cded4
Andres Avendaño <afavendanora@gmail.com>
2025-09-03 23:18:57 -0500

    feat: Ajuste mensaje de error

--- domain/model/src/main/java/co/com/pragma/model/error/ResponseCode.java
+++ domain/model/src/main/java/co/com/pragma/model/error/ResponseCode.java
@@ -7,7 +7,7 @@ public enum ResponseCode {
     MSSO003("La entidad a registrar ya existe en la app."),
     MSSO004("No se encontraron registros con los datos ingresados."),
     MSSO005("Identificación no es válida o no existe en el token."),
-    MSSO006("Solo puede crear sus porpias solicitudes."),
+    MSSO006("Solo puede crear sus propias solicitudes."),
     MSSO007("Error obteniendo información del token.");
 
     private final String message;
